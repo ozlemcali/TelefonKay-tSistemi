563cbbe02bb30ff2a5e4ae016ddb09aa93b124a9
Özlem <zlem@DESKTOP-086LPK4>
2021-03-03 22:20:40 +0300

    Add project files.

---
+++ TelefonKayıtSistemi/telefonKayit.cpp
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include"telefonKayit.h"
+
+using namespace std;
+int telefonKayit::sayac = 0;
+void telefonKayit::setIsim () {
+	cout << "Isim:" << endl;
+	cin >> ad;
+}
+void telefonKayit::setSoyad () {
+	cout << "Soyisim:" << endl;
+	cin >> soyad;
+}
+void telefonKayit::setCepTel () {
+	cout << "Cep telefonu:" << endl;
+	cin >> cepTel;
+}
+void telefonKayit::setIsTel () {
+	cout << "Is telefonu:" << endl;
+	cin >> isTel;
+}
+void telefonKayit::kisiEkle () {
+	system ("cls");
+	setIsim ();
+	setSoyad ();
+	setCepTel ();
+	setIsTel ();
+	sayac++;
+	ID = sayac;
+}
+
+string telefonKayit::getAd () {
+	return ad;
+}
+string telefonKayit::getSoyad () {
+	return soyad;
+}
+unsigned long long int telefonKayit::getCepTel () {
+	return cepTel;
+}
+unsigned long long int telefonKayit::getÝsTel () {
+	return isTel;
+}
+int telefonKayit::getID () {
+	return ID;
+}
